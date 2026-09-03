0271e45ee7de02a2cf6662f2c5a206604f000de4
Osanna Hui <osanna.hui@gmail.com>
2026-09-03 15:28:22 -0700

    update frontend with mocks

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,3 +8,27 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>,
 );
+
+/**
+ *
+ * Use the following to render the application
+ * if `msw` is needed to complete the Frontend coding exercise
+ *
+ */
+// async function startMocks() {
+//   const { worker } = await import("./mocks/browser");
+//   await worker.start({
+//     serviceWorker: {
+//       url: "/mockServiceWorker.js",
+//     },
+//   });
+// }
+
+// startMocks().then(() => {
+//   const root = ReactDOM.createRoot(document.getElementById("root")!);
+//   root.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>,
+//   );
+// });
